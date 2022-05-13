d8aa674fd1c7cff749a2e0aab48858ae529de0b3
luochun <luochun15@gmail.com>
2022-05-13 21:02:09 +0800

    编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
  <!-- CSRF Token -->
  <meta name="csrf-token" content="{{ csrf_token() }}">
  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+ @yield('styles')
  <!-- Styles -->
  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
@@ -25,6 +26,6 @@
 
  <!-- Scripts -->
  <script src="{{ mix('js/app.js') }}"></script>
-
+@yield('scripts')
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,9 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .version()
-    .sourceMaps();
+    .sourceMaps()
+    .copyDirectory('resources/editor/js', 'public/js')
+    .copyDirectory('resources/editor/css', 'public/css');
 mix.webpackConfig({
         stats: {
             children: true,
