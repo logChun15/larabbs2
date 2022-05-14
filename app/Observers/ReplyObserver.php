cee38e0fc6d5d8cd5ef0c4b2442727cffd6dcd25
luochun <luochun15@gmail.com>
2022-05-14 13:03:06 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,11 +11,24 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
+
     }
 
     public function updating(Reply $reply)
     {
         //
     }
+
+    public function created(Reply $reply)
+    {
+        // $reply->topic->increment('reply_count', 1);
+        // 上面自增 1 是比较直接的做法，另一个比较严谨的做法是创建成功后计算本话题下评论总数，然后在对其
+        // reply_count 字段进行赋值。
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+    }
+
+
 }
