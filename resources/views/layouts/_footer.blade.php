ce120ce2f6711452656dd15b86ed40abec1d9698
luochun <luochun15@gmail.com>
2022-05-15 10:12:57 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
              <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">luochun的</a> Seaflix <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="float-end"><a href="luochun@mail.com">联系我们</a></p>
+        <p class="float-end"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
